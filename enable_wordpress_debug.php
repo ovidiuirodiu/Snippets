2eec792586817cd0992c02a688c42f589e8bc047
mariusvetrici <mariusvetrici@users.noreply.github.com>
2016-07-26 15:24:17 +0300

    Added description
    Enable WordPress debug to file in wp-content/debug.log
    Add this to wp-config.php

--- enable_wordpress_debug.php
+++ enable_wordpress_debug.php
@@ -2,4 +2,4 @@
 // Pug this in wp_config.php
 define('WP_DEBUG',         true);  // Turn debugging ON
 define('WP_DEBUG_DISPLAY', false); // Turn forced display OFF
-define('WP_DEBUG_LOG',     true);  // Turn logging to wp-content/debug.log ON
+define('WP_DEBUG_LOG',     true);  // Turn logging to wp-content/debug.log  ON
